4288daaa885c1f23454ff79f328033dc9aa8705b
Pedro Araujo <91104140+Araujopedro@users.noreply.github.com>
2024-04-01 17:19:49 -0300

    pasta do smodulos de Acessabilidade

---
+++ Acessibilidade/page.tsx
@@ -0,0 +1,12 @@
+
+import '../Acessibilidade/acessibilidade.module.css'
+
+const Acessibilidade = () => {
+    return (
+        <div className='poppins-bold'>
+            
+        </div>
+    );
+};
+
+ export default Acessibilidade;
